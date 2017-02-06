48fe9096be1dc2a603793d6ea66bccfce5de0186
Vytautas Kavaliauskas <vytautas.k@datadog.org>
2017-02-06 10:26:21 +0200

    Create gir-recorder.sh

---
+++ gir-recorder.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install xrectsel (from https://github.com/lolilolicon/xrectsel/) and byzanz (sudo apt-get install byzanz)
+# Throw this script somewhere and make an alias (I called mine gif)
+
+DATE=`date +%Y%m%d%H%M%S`
+
+DEFAULT_DURATION=10
+
+echo -ne "Select area which will be recorded"
+
+AREA="$(xrectsel)"
+IFS=+ read DIM X Y <<< $AREA
+IFS=x read W H <<< $DIM
+
+echo ""
+
+read -p "Enter recording duration in seconds: " USERDUR
+
+# Duration and output file
+if [ $USERDUR -gt 0 ]; then
+    DURATION=$USERDUR
+else
+    DURATION=$DEFDUR
+fi
+
+echo "Recording has started..."
+
+byzanz-record --duration=$DURATION --x=$X --y=$Y --width=$W --height=$H ~/Pictures/out${DATE}.gif
+
+echo "Recording has ended."
+
+eog ~/Pictures/out${DATE}.gif
